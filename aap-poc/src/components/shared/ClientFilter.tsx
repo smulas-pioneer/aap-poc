fa970acd916d2c5bb0eea94bc0d20e9636e33441
Ruska2 <ruska2@RSK-IMAC.local>
2017-11-17 17:54:28 +0100

    rsk

--- aap-poc/src/components/shared/ClientFilter.tsx
+++ aap-poc/src/components/shared/ClientFilter.tsx
@@ -61,7 +61,7 @@ export class ClientFilter extends React.Component<ClientFilterProps, ClientFilte
 
     hasMoreThenOneFilter(values: SearchFilter) { return Object.keys(values).filter(e => values[e].isInUse).length > 1; }
 
-    renderFilterMenuNew(key: number, map: FilterMap, values: SearchFilter) {
+    renderFilterMenu(key: number, map: FilterMap, values: SearchFilter) {
         const { searchprop, render: { icon, header, label, max }, enableClearAll = true } = map;
 
         const { renderAllFilters } = this.state;
@@ -122,77 +122,21 @@ export class ClientFilter extends React.Component<ClientFilterProps, ClientFilte
         );
     }
 
-    renderFilterMenu(key: number, map: FilterMap, values: SearchFilter) {
-        const { searchprop, render: { icon, header, label, max }, enableClearAll = true } = map;
 
-        const keys = values && Object.keys(values).sort((a, b) => {
-            if (b.startsWith('<1')) return 1;
-            if (b.startsWith('>20')) return -1;
-            if (a.startsWith('10-')) return 1;
-            if (a < b) return -1;
-            if (a > b) return 1;
-            return 0;
-        }).reduce((memo, prop, j) => {
-            const item = (
-                <Menu.Item key={j} name='prop' link onClick={() => searchprop && this.searchAdvanced(searchprop, prop, values[prop].isInUse)}>
-                    {values[prop].current > 0 && <Label size="tiny" content={values[prop].current} />}
-                    {values[prop].isInUse && <Icon name="checkmark" color="green" style={{ float: 'none', margin: '0 .5em 0 0' }} />}
-                    {prop !== '0' ? (label ? label(prop) : prop) : 'none'}
-                </Menu.Item>
-            );
-
-            if (max && j > max) {
-                memo.others.push(item);
-            } else {
-                memo.menu.push(item);
-            }
-
-            return memo;
-        }, { menu: [], others: [] } = { menu: [] as any, others: [] as any });
-
-        return (keys &&
-            <Menu.Item key={key}>
-                <Menu.Header>
-                    <Icon name={icon} />{header}
-                    {enableClearAll && this.hasMoreThenOneFilter(values) && <IconButton color="red" name="remove" floated="right" onClick={() => this.clearAllFilters(searchprop)} />}
-                </Menu.Header>
-                <Menu.Menu>
-                    {keys.menu}
-                    {keys.others.length ?
-                        (
-                            <Accordion defaultActiveIndex={undefined} panels={[
-                                {
-                                    key: 'others',
-                                    title: { as: Menu.Item, content: <Segment as="span" style={{ paddingLeft: 0 }} basic><label>...others</label></Segment> },
-                                    content: {
-                                        content: (
-                                            <Menu.Menu>
-                                                {keys.others}
-                                            </Menu.Menu>
-                                        )
-                                    }
-                                }
-                            ]} />
-                        ) : null}
-                </Menu.Menu>
-
-            </Menu.Item>
-        );
-    }
 
     renderFilter() {
         let ret: JSX.Element[] = [];
 
         if (isArray(this.props.filterMaps)) {
             this.props.filterMaps.forEach((filter, i) =>
-                ret.push(this.renderFilterMenuNew(i, filterMapItems[filter], this.props.data![filter]))
+                ret.push(this.renderFilterMenu(i, filterMapItems[filter], this.props.data![filter]))
             );
         } else {
             Object.keys(this.props.filterMaps).forEach((filter, i) => {
                 const fieldMap: FilterMap = {
                     ...filterMapItems[filter], enableClearAll: this.props.filterMaps[filter].clearAll
                 };
-                ret.push(this.renderFilterMenuNew(i, fieldMap, this.props.data![filter]));
+                ret.push(this.renderFilterMenu(i, fieldMap, this.props.data![filter]));
             });
         }
 
