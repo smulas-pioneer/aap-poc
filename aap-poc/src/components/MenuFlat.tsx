63b99141fe5116a1c12c782a4b85aaec6cf13c77
Daniele Giove <daniele.giove@pioneerinvestments.com>
2017-11-17 12:06:41 +0100

    dg

--- aap-poc/src/components/MenuFlat.tsx
+++ aap-poc/src/components/MenuFlat.tsx
@@ -111,7 +111,7 @@ class MenuFlat extends conn.StatefulCompo<MenuFlatState> {
 
                     <img src={logo2} />
 
-                    <Menu.Item color="blue" icon={logo2} to="/">
+                    <Menu.Item color="blue" icon={logo2} as={Link} to="/">
                         <h2 style={{ color: '#005483', fontFamily: 'HelveticaNeue' }} > Amundi Digital Advisory </h2>
                     </Menu.Item>
 
