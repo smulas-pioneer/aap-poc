8772068fdac8e89258470cfe3420563dde2f2000
Daniele Giove <daniele.giove@pioneerinvestments.com>
2018-01-15 16:37:39 +0100

    dg

--- aap-poc/src/components/Dashboard.tsx
+++ aap-poc/src/components/Dashboard.tsx
@@ -112,7 +112,7 @@ class Dashboard extends conn.StatefulCompo<DashboardState> {
         )
     }
 
-    Colors = ["#F07D00", "#004F9F", "#E6325E", "#3B7296", "#39B2B6"]
+    Colors = ["#F07D00", "#004F9F", "#E6325E", "#3B7296", "#39B2B6", "#c8c802"]
 
     renderFilterGraphItem(key: number, map: FilterMap, values: any) {
         const { searchprop, render: { header, label } } = map;
