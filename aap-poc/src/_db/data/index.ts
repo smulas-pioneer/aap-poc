cb52612b99454b9081ac6d657848ece18c66a5ee
Simone Mulas <simo@SM-IMAC.local>
2018-02-14 11:38:17 +0100

    added config.json

--- aap-poc/src/_db/data/index.ts
+++ aap-poc/src/_db/data/index.ts
@@ -11,53 +11,59 @@ export const group = <T>(data: T[], key: keyof (T)) => data.reduce(
     {} as { [id: string]: T }
 );
 
+
+
+
 let radars: { [id: string]: Radar };
-fetch('/radars.json').then(r => r.json()).then(p => radars = p);
-
-//PERFORMANCES
 let performances: { [isin: string]: { date: string, perf: number }[] };
-fetch('/performances.json').then(r => r.json()).then(p => performances = p);
-
 let perfSummary: { [isin: string]: { date: string, perf: number }[] };
-fetch('/perfSummary.json').then(r => r.json()).then(p => perfSummary = p);
-
-//SECURITIES
 let securities2: Security[];
-fetch('/securities2.json').then(r => r.json()).then(p => {
-    securities2 =
-        (securityList).map((p:any) => ({ ...p, blacklisted: p.Rating == "BBB" && p.Sector == "Utilities" }))
-            .concat(p)
-            .map((r:any)=> ({
-                ...r, pushed:
-                    r.SecurityName.toLowerCase().indexOf("amundi") > -1 ||
-                    r.SecurityName.toLowerCase().indexOf("pioneer") > -1
-            }))
-});
-
-//SECURITIES
 let clients: Client[];
-fetch('/clients.json').then(r => r.json()).then(p => {
-    clients = p
-});
-
-//STRATEGIES
 let strategies: { [isin: string]: StrategyItem[] };
-fetch('/strategy.json').then(r => r.json()).then(p => {
-    strategies = p
-    updateStrategies();
-});
-
-//ALERT HISTORY
 let alertHistory: AlertHistory[];
-fetch('/alertHistory.json').then(r => r.json()).then(p => alertHistory = p);
-
-//History
 let history: { [isin: string]: InterviewResult[] };
-fetch('/history.json').then(r => r.json()).then(p => history = p);
-
-//ALERT HISTORY
 let agents: string[];
-fetch('/agents.json').then(r => r.json()).then(p => agents = p);
 
+export const loadDatabase = (appName: string) => {
+
+    fetch(appName + '/radars.json').then(r => r.json()).then(p => radars = p);
+
+    //PERFORMANCES
+    fetch(appName + '/performances.json').then(r => r.json()).then(p => performances = p);
+
+    fetch(appName + '/perfSummary.json').then(r => r.json()).then(p => perfSummary = p);
+
+    //SECURITIES
+    fetch(appName + '/securities2.json').then(r => r.json()).then(p => {
+        securities2 =
+            (securityList).map((p: any) => ({ ...p, blacklisted: p.Rating == "BBB" && p.Sector == "Utilities" }))
+                .concat(p)
+                .map((r: any) => ({
+                    ...r, pushed:
+                        r.SecurityName.toLowerCase().indexOf("amundi") > -1 ||
+                        r.SecurityName.toLowerCase().indexOf("pioneer") > -1
+                }))
+    });
+
+    //SECURITIES
+    fetch(appName + '/clients.json').then(r => r.json()).then(p => {
+        clients = p
+    });
+
+    //STRATEGIES
+    fetch(appName + '/strategy.json').then(r => r.json()).then(p => {
+        strategies = p
+        updateStrategies();
+    });
+
+    //ALERT HISTORY
+    fetch(appName + '/alertHistory.json').then(r => r.json()).then(p => alertHistory = p);
+
+    //History
+    fetch(appName + '/history.json').then(r => r.json()).then(p => history = p);
+
+    //ALERT HISTORY
+    fetch(appName + '/agents.json').then(r => r.json()).then(p => agents = p);
+}
 
 export { radars, securities2 as securityList, clients as clientList, history, agents, strategies, performances, alertHistory, perfSummary };
--- aap-poc/src/index.tsx
+++ aap-poc/src/index.tsx
@@ -4,22 +4,27 @@ import { render } from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import * as reducers from './reducers';
-import { configureStore, HashRouter as Router } from 'app-support';
+import { configureStore, HashRouter as Router, loadConfiguration } from 'app-support';
 
 import './styles/index.css';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/hovereffect.css';
 import { Root } from './components/Root';
 import { AppState } from './reducers';
+import { loadDatabase } from './_db/data';
 
 const startingState: any = {}
 
 const store = configureStore(reducers.default, /*startingState,*/ true);
-render(
-    <Provider store={store}>
-        <Router >
-            <Root />
-        </Router>
-    </Provider>,
-    document.getElementById('root')
-);
+
+loadConfiguration('config.json').then((cfg: { APPNAME: string }) => {
+    loadDatabase(cfg.APPNAME);
+    render(
+        <Provider store={store}>
+            <Router>
+                <Root />
+            </Router>
+        </Provider>,
+        document.getElementById('root')
+    );
+});
