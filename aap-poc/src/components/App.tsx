5e98a16572fa1e1241b151d97da97f5fe79f9daf
Daniele Giove <jodan-pz@gmail.com>
2019-06-27 16:10:20 +0200

    dg

--- aap-poc/src/components/App.tsx
+++ aap-poc/src/components/App.tsx
@@ -11,28 +11,31 @@ import { ManagerView } from './managerView/managerView';
 import { DashboardMgr } from './DashboardMgr';
 
 interface IAppProps {
-    manager?: boolean
+  manager?: boolean
 }
 
+/**
+ * Main App
+ */
 class App extends React.Component<IAppProps, {}> {
-    render() {
-        const { manager } = this.props;
-        return (
-            <div className="app" style={{ backgroundColor: '#f8f5f5', height: '100vh', width: '100%', position: 'absolute', top: 0, bottom: 0 }}>
-            <Route exact={false} path="/" component={(p: RouteComponentProps<any>) => <MenuFlat history={p.history}/>}/>
-            <Segment basic style={{ marginTop: 0, height: '95%' }}>
-                    {/* <Route exact path="/" component={(p: any) => <HomePage />} />
+  render() {
+    const { manager } = this.props;
+    return (
+      <div className="app" style={{ backgroundColor: '#f8f5f5', height: '100vh', width: '100%', position: 'absolute', top: 0, bottom: 0 }}>
+        <Route exact={false} path="/" component={(p: RouteComponentProps<any>) => <MenuFlat history={p.history} />} />
+        <Segment basic style={{ marginTop: 0, height: '95%' }}>
+          {/* <Route exact path="/" component={(p: any) => <HomePage />} />
                     <Route exact path="/homepage" component={(p: any) => <HomePage />} /> */}
-                    <Route exact path="/" component={(p: any) => manager ? <DashboardMgr uid="dashboard" /> : <Dashboard uid="dashboard" />} />
-                    <Route exact path="/clients" component={(p: any) => <ClientsView uid="cli" showFilter showTitle />} />
-                    <Route exact path="/alerts" component={(p: any) => <AlertsView uid="alerts" manager={manager} showFilter showTitle />} />
-                    <Route exact path="/manager" component={(p: any) => <ManagerView uid="port" showFilter showTitle />} />
-                    <Route exact path="/agents/:id/clients" component={(p: RouteComponentProps<any>) => <ClientsView uid="agecli" agent={p.match.params.id} showFilter showTitle />} />
-                    <Route exact path="/clients/:id" component={(p: RouteComponentProps<any>) => <ClientView id={p.match.params.id} />} />
-                </Segment>
-            </div>
-        );
-    }
+          <Route exact path="/" component={(p: any) => manager ? <DashboardMgr uid="dashboard" /> : <Dashboard uid="dashboard" />} />
+          <Route exact path="/clients" component={(p: any) => <ClientsView uid="cli" showFilter showTitle />} />
+          <Route exact path="/alerts" component={(p: any) => <AlertsView uid="alerts" manager={manager} showFilter showTitle />} />
+          <Route exact path="/manager" component={(p: any) => <ManagerView uid="port" showFilter showTitle />} />
+          <Route exact path="/agents/:id/clients" component={(p: RouteComponentProps<any>) => <ClientsView uid="agecli" agent={p.match.params.id} showFilter showTitle />} />
+          <Route exact path="/clients/:id" component={(p: RouteComponentProps<any>) => <ClientView id={p.match.params.id} />} />
+        </Segment>
+      </div>
+    );
+  }
 }
 
 export default App;
