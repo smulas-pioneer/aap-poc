47810a1ad738dd69ebb3debf71f0ed81ca24f16f
Simone Mulas <simone.mulas@gmail.com>
2019-07-02 12:15:36 +0200

    sm

--- aap-poc/src/_db/coreEngine.ts
+++ aap-poc/src/_db/coreEngine.ts
@@ -24,7 +24,7 @@ const getSuggestion1 = (position: StrategyItem[], axes: RadarStrategyParm, calcu
         if ( strategy ) {
           return { ...p, suggestionAccepted: strategy.suggestionAccepted }
         }
-        console.log('bah');
+      console.log('ERROR', strategy,forced.map(i=>i.security.IsinCode), position.map(i=>i.security.IsinCode),p.security.IsinCode);
         return p;
       })
       : position;
